51e2648458959adaebfd22e1d2a270688fc79be8
wpjgit <18829234984@163.com>
2017-05-30 18:15:42 +0800

    bencode编码已实现，未测试

--- dht/bencode_test.go
+++ dht/bencode_test.go
@@ -1,1 +1,13 @@
-package dht 
+package dht
+
+import (
+	"testing"
+)
+
+func test_EncodeBencode(t *testing.T) {
+	var m = map[string]interface{}{"1": "a", "2": "b"}
+	a, error := EncodeBencode(m)
+	if error == nil {
+		//fmt.Println(string(a) + "--" + string(error))
+	}
+}
