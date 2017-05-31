470f932dfa09f77c15d64f5237100c5dfc7be4ba
l158aaa <18829236015@163.com>
2017-05-31 18:53:13 +0800

    bencode 完成, 已经测试

--- dht/bencode.go
+++ dht/bencode.go
@@ -1,28 +1,40 @@
 package dht
 
 import (
-	"bytes"
-	"encoding/binary"
 	"errors"
 	"strconv"
 	"strings"
 )
 
-func main() {
-	s := "123"
-	print(strings.ContainsRune(s, 1))
-}
-
-// EncodeBencode bencode 编码
-func EncodeBencode(msg map[string]interface{}) ([]byte, error) {
+// encodeBencode bencode 编码
+func encodeBencode(msg map[string]interface{}) ([]byte, error) {
 	if msg == nil {
 		return nil, errors.New("msg is null")
 	}
-	return []byte(encodeDict(msg)), nil
+	var err error
+	str, err := encodeDict(msg)
+	if err != nil {
+		return nil, errors.New("EncodeBencode")
+	}
+	return []byte(str), nil
+}
+
+// decodeBencode bencode 解码
+func decodeBencode(msg []byte) (map[string]interface{}, error) {
+	if msg == nil {
+		return nil, errors.New("msg is null")
+	}
+	var result map[string]interface{}
+	var err error
+	result, _, err = decodeDict(msg, 0)
+	if err != nil {
+		return nil, errors.New("DecodeBencode")
+	}
+	return result, nil
 }
 
 // encodeInterface
-func encodeInterface(data interface{}) string {
+func encodeInterface(data interface{}) (string, error) {
 	switch data.(type) {
 	case int:
 		return encodeInt(data.(int))
@@ -33,46 +45,47 @@ func encodeInterface(data interface{}) string {
 	case map[string]interface{}:
 		return encodeDict(data.(map[string]interface{}))
 	default:
-		panic("invalid type when encode")
+		return "error", errors.New("invalid type when encode")
 	}
 }
 
 // 将int类型转化为bencode编码
-func encodeInt(data int) string {
-	return strings.Join([]string{"i", "e"}, strconv.Itoa(data))
+func encodeInt(data int) (string, error) {
+	return strings.Join([]string{"i", "e"}, strconv.Itoa(data)), nil
 }
 
 // 将string类型转化为bencode编码
-func encodeString(data string) string {
-	return strings.Join([]string{strconv.Itoa(len(data)), data}, ":")
+func encodeString(data string) (string, error) {
+	return strings.Join([]string{strconv.Itoa(len(data)), data}, ":"), nil
 }
 
 // 将list类型转化为bencode编码
-func encodeList(data []interface{}) string {
+func encodeList(data []interface{}) (string, error) {
 	result := ""
 	for _, item := range data {
-		result = strings.Join([]string{result, encodeInterface(item)}, "")
+		var err error
+		str, err := encodeInterface(item)
+		if err != nil {
+			return "error", errors.New("encodeList")
+		}
+		result = strings.Join([]string{result, str}, "")
 	}
-	return strings.Join([]string{"l", result, "e"}, "")
+	return strings.Join([]string{"l", result, "e"}, ""), nil
 }
 
 // 将map类型转化为bencode编码
-func encodeDict(data map[string]interface{}) string {
+func encodeDict(data map[string]interface{}) (string, error) {
 	result := ""
 	for key, item := range data {
-		result = strings.Join([]string{result, encodeString(key), encodeInterface(item)}, "")
+		str1, _ := encodeString(key)
+		var err error
+		str2, err := encodeInterface(item)
+		if err != nil {
+			return "error", errors.New("encodeDict")
+		}
+		result = strings.Join([]string{result, str1, str2}, "")
 	}
-	return strings.Join([]string{"d", result, "e"}, "")
-}
-
-// DecodeBencode bencode 解码
-func DecodeBencode(msg []byte) (map[string]interface{}, error) {
-	if msg == nil {
-		return nil, errors.New("msg is null")
-	}
-	var result map[string]interface{}
-	result, _, _ = decodeDict(msg, 0)
-	return result, nil
+	return strings.Join([]string{"d", result, "e"}, ""), nil
 }
 
 // decodeInterface
@@ -87,34 +100,35 @@ func decodeInterface(data []byte, start int) (interface{}, int, error) {
 	case 'd':
 		return decodeDict(data, start)
 	default:
-		panic("invalid type when decode")
+		return nil, -1, errors.New("invalid type when encode")
 	}
 }
 
 // 解码int类型
 func decodeInt(data []byte, start int) (int, int, error) {
 	end := start + 1
-	for data[end] == 'e' {
+	for data[end] != 'e' {
 		end++
 		if end == len(data) {
 			return -1, -1, errors.New("e not found when decode int")
 		}
 	}
-	return bytesToInt(data[start+1 : end]), end + 1, nil
+	num, _ := strconv.Atoi(string(data[start+1 : end]))
+	return num, end + 1, nil
 }
 
 // 解码string类型
 func decodeString(data []byte, start int) (string, int, error) {
 	middle := start + 1
-	for data[middle] == ':' {
+	for data[middle] != ':' {
 		middle++
 		if middle == len(data) {
 			return "", -1, errors.New("： not found when decode string")
 		}
 	}
-	len := bytesToInt(data[start:middle])
+	len, _ := strconv.Atoi(string(data[start:middle]))
 	end := middle + len + 1
-	return string(data[middle+1 : end]), end + 1, nil
+	return string(data[middle+1 : end]), end, nil
 }
 
 // 解码List类型
@@ -123,7 +137,11 @@ func decodeList(data []byte, start int) ([]interface{}, int, error) {
 	end := start + 1
 	for end < len(data) {
 		var item interface{}
-		item, end, _ = decodeInterface(data, end)
+		var err error
+		item, end, err = decodeInterface(data, end)
+		if err != nil {
+			return nil, -1, errors.New("decodeList")
+		}
 		list = append(list, item)
 		if data[end] == 'e' {
 			break
@@ -142,34 +160,35 @@ func decodeDict(data []byte, start int) (map[string]interface{}, int, error) {
 	for end < len(data) {
 		var key interface{}
 		var item interface{}
-		key, end, _ = decodeInterface(data, end)
-		switch item.(type) {
+		var err1 error
+		key, end, err1 = decodeInterface(data, end)
+		if err1 != nil {
+			return nil, -1, errors.New("invalid type of dictionary")
+		}
+		switch key.(type) {
 		case string:
 		default:
 			return nil, -1, errors.New("invalid type of dictionary")
 		}
+
 		if data[end] == 'e' {
 			return nil, -1, errors.New("invalid type of dictionary")
 		}
 		if end == len(data) {
 			return nil, -1, errors.New("e not found when decode list")
 		}
-		item, end, _ = decodeInterface(data, end)
-		dict[key.(string)] = item
-		if data[end] == 'e' {
-			break
+		var err2 error
+		item, end, err2 = decodeInterface(data, end)
+		if err2 != nil {
+			return nil, -1, errors.New("invalid type of dictionary")
 		}
+		dict[key.(string)] = item
 		if end == len(data) {
 			return nil, -1, errors.New("e not found when decode list")
 		}
+		if data[end] == 'e' {
+			break
+		}
 	}
 	return dict, end + 1, nil
-}
-
-// 字节数组转int
-func bytesToInt(b []byte) int {
-	byteBuffer := bytes.NewBuffer(b)
-	var tmp int
-	binary.Read(byteBuffer, binary.BigEndian, &tmp)
-	return tmp
 }
